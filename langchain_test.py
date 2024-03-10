ba75d2e4cbd9befc2486cb4593b7c1a4b7401f22
GeniusMundi10 <159649324+GeniusMundi10@users.noreply.github.com>
2024-03-11 01:39:05 +0530

    Update langchain_test.py

--- langchain_test.py
+++ langchain_test.py
@@ -27,7 +27,7 @@ chain = model | parser
 st.title("ELT LM GPT")
 
 template = """
-Answer the question based on the context below. If you can't answer the question reply "I dont Know". Keep the answer as detailed as possible.
+Answer the question based on the context below. If you can't answer the question reply "I dont Know". Keep the answer in discursive.
 
 Context:{context}
 
